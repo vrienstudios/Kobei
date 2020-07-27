90068ff1ac751d8d3864768983c3b0d4c2b265b2
ShujianDou <visionistprojects@gmail.com>
2020-07-27 15:49:21 -0400

    ChapterView update

--- ChapterViewer.cpp
+++ ChapterViewer.cpp
@@ -3,4 +3,6 @@
 ChapterView::ChapterView()
 {
 	chapterBuffer = gcnew VTable(Chapter::typeid);
+	rtb = gcnew System::Windows::Forms::RichTextBox;
+	rtb->Dock = System::Windows::Forms::DockStyle::Fill;
 }
--- ChapterViewer.h
+++ ChapterViewer.h
@@ -4,6 +4,8 @@
 ref class ChapterView : System::Windows::Forms::Panel {
 public:
 	VTable^ chapterBuffer; // index 1 is always the current, 0 is the past chapter, 2 is the future chapter.
+	System::Windows::Forms::RichTextBox^ rtb;
+
 
 	ChapterView();
 	~ChapterView() {
