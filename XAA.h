d029731cfc6ac6c25aa7e6197b497458174e680d
ShujianDou <visionistprojects@gmail.com>
2020-08-03 23:38:11 -0400

    XAA - better data management setup

---
+++ XAA.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Functions.h"
+#include "Includes.h"
+struct XAA {
+	System::String^ DeSerialize(System::Object^ clss) {
+		System::Type^ t = clss->GetType();
+		cli::array<System::Reflection::FieldInfo^>^ pi = t->GetFields();
+		System::Text::StringBuilder^ sBuilder = gcnew System::Text::StringBuilder();
+		sBuilder->AppendLine(("<" + t->Name + ">"));
+		for (int idx = 0; idx < pi->Length; idx++)
+			sBuilder->AppendLine(pi[idx]->Name + ":" + pi[idx]->GetValue(clss)->ToString());
+		sBuilder->AppendLine(("</" + t->Name + ">"));
+		return sBuilder->ToString();
+	}
+
+	System::Object^ Serialize(cli::array<System::String^>^ xaa, System::Type^ clss) {
+		System::Object^ m = System::Activator::CreateInstance(clss);
+		for (int idx = 1; idx < xaa->Length - 2; idx++) {
+			cli::array<System::String^>^ tokenSplit = xaa[idx]->Split(':');
+			switch (Functions::SwitchString(msclr::interop::marshal_as<std::string>(clss->GetField(tokenSplit[0])->FieldType->ToString()).c_str())) {
+			case Functions::SwitchString("System.Int32"): {
+				break;
+			}
+			case Functions::SwitchString("System.String"): {
+				break;
+			}
+			}
+
+		}
+	}
+};
