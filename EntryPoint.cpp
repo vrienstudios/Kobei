282859d62d65258807094087b95eac94b1589ba2
ShujianDou <visionistprojects@gmail.com>
2020-08-04 01:15:25 -0400

    Removed Bloat from entrypoint

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -12,17 +12,6 @@ int main(int argc, char* argv[]) {
 		std::cout << "Found Arguments... Reading.";
 	}
 	else {
-		//std::cout << "Loading!";
-		//Book^ b = BookAddForm::GetBook(TRUE);
-		//Book^ b = gcnew Book(nullptr, "kev", "Chay", "2020");
-		/*Volume^ Vol = gcnew Volume();
-		Vol->Name = "Kevo";
-		Vol->attachedBook = b;
-		Chapter^ chp = gcnew Chapter();
-		chp->Name = "1 Ckao";
-		chp->Text = "LERO";
-		Vol->ChapterList->Add(0, chp);
-		BookAddForm::ExportVolumeFull(Vol);*/
 		Window^ win = gcnew Window(800, 600);
 		win->Show();
 	}
